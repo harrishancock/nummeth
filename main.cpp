0c86ada7761010f3409655f972cdc5fec9d81e78
Harris Hancock <vortrab@gmail.com>
2013-03-28 12:07:48 -0700

    Add some more operations

--- main.cpp
+++ main.cpp
@@ -3,39 +3,109 @@
 #include <cstdio>
 
 int main () {
-    polynomial p { { 2.5, 0, 12 } };
+    polynomial p { { 12, 0, 2.5 } };
     printf("p(1) == %f\n", p(1));
+    printf("p(1.1) == %f\n", p(1.1));
+    printf("p(1.2) == %f\n", p(1.2));
+    printf("p(1.3) == %f\n", p(1.3));
     printf("p(3) == %f\n", p(3));
     printf("p(8) == %f\n", p(8));
     polynomial dp { p.derivative() };
     printf("dp(1) == %f\n", dp(1));
 
-    polynomial p0 { { 1, -12, 0, -42 } };
-    polynomial p1 { { 1, -3 } };
-    polynomial p2 { { 1, 1, -3 } };
+    polynomial p0 { { -42, 0, -12, 1 } };
 
-    polynomial p3 { { 6, 5, 0, -7 } };
-    polynomial p4 { { 3, -2, -1 } };
+
+    polynomial p1 { { -3, 1 } };
+    polynomial p2 { { -3, 1, 1 } };
+
+    polynomial p3 { { -7, 0, 5, 6 } };
+    polynomial p4 { { -1, -2, 3 } };
+
+    std::cout << "p0 == " << p0 << '\n';
+    std::cout << "p1 == " << p1 << '\n';
+    std::cout << "p2 == " << p2 << '\n';
+    std::cout << "p3 == " << p3 << '\n';
+    std::cout << "3*p0 == " << 3*p0 << '\n';
+    std::cout << "p0 + p1 == " << p0 + p1 << '\n';
+    std::cout << "p0 + p2 == " << p0 + p2 << '\n';
+    std::cout << "p0 + p3 == " << p0 + p3 << '\n';
+    std::cout << "p0 - p1 == " << p0 - p1 << '\n';
+    std::cout << "p0 - p2 == " << p0 - p2 << '\n';
+    std::cout << "p0 - p3 == " << p0 - p3 << '\n';
+    std::cout << "p0 * p1 == " << p0 * p1 << '\n';
+    std::cout << "p0 * p2 == " << p0 * p2 << '\n';
+    std::cout << "p0 * p3 == " << p0 * p3 << '\n';
 
     {
-        auto qr = p0 / p1;
+        auto qr = p0.quo_rem(p1);
         const polynomial& q = qr.first;
         const polynomial& r = qr.second;
         std::cout << p0 << " / " << p1 << " == (" << q << ", " << r << ")\n";
     }
 
     {
-        auto qr = p0 / p2;
+        auto qr = p0.quo_rem(p2);
         const polynomial& q = qr.first;
         const polynomial& r = qr.second;
         std::cout << p0 << " / " << p2 << " == (" << q << ", " << r << ")\n";
     }
 
     {
-        auto qr = p3 / p4;
+        auto qr = p3.quo_rem(p4);
         const polynomial& q = qr.first;
         const polynomial& r = qr.second;
         std::cout << p3 << " / " << p4 << " == (" << q << ", " << r << ")\n";
+    }
+
+    polynomial sturm_p { { -1, -1, 0, 1, 1 } };
+    std::cout << "Sturm Chain of (" << sturm_p << ")\n";
+
+    auto sturm_c = sturm_chain(sturm_p);
+    for (auto p : sturm_c) {
+        std::cout << p << '\n';
+    }
+
+    {
+        printf("p[i] at -inf: ");
+        int sigma = 0;
+        bool last_sign = sturm_c[0][sturm_c[0].degree()] < 0;
+        last_sign = sturm_c[0].degree() & 1 ? !last_sign : last_sign;
+        for (auto p : sturm_c) {
+            bool sign = p[p.degree()] < 0;
+            sign = p.degree() & 1 ? !sign : sign;
+            sigma += last_sign != sign;
+            last_sign = sign;
+            printf("%c ", sign ? '-' : '+');
+        }
+        printf("(%d)\n", sigma);
+    }
+
+    {
+        printf("p[i] at inf: ");
+        int sigma = 0;
+        bool last_sign = sturm_c[0][sturm_c[0].degree()] < 0;
+        for (auto p : sturm_c) {
+            bool sign = p[p.degree()] < 0;
+            sigma += last_sign != sign;
+            last_sign = sign;
+            printf("%c ", sign ? '-' : '+');
+        }
+        printf("(%d)\n", sigma);
+    }
+
+    {
+        printf("Legendre polynomials:\n");
+        polynomial x { { 0, 1 } };
+        std::vector<polynomial> legendre (10, polynomial { { 0.0 } });
+        legendre[0] = polynomial { { 1 } };
+        legendre[1] = x;
+        for (int i = 2; i < legendre.size(); i++) {
+            legendre[i] = ((2*i - 1)*x*legendre[i-1] - (i - 1)*legendre[i-2]) * (1.0/i);
+        }
+        for (auto p : legendre) {
+            std::cout << p << '\n';
+        }
     }
 
     return 0;
--- polynomial.hpp
+++ polynomial.hpp
@@ -1,69 +1,136 @@
 #ifndef NUMMETH_POLYNOMIAL_HPP
 #define NUMMETH_POLYNOMIAL_HPP
 
+#include <boost/range/adaptor/reversed.hpp>
+#include <boost/range/combine.hpp>
+
 #include <vector>
 #include <iostream>
 
 class polynomial {
 public:
+    polynomial () : coefficients{0} { }
+
     explicit polynomial (std::vector<double> cs) : coefficients(cs) { }
 
     double operator() (const double x) const {
         double p = 0;
-        for (auto c : coefficients) {
+        for (auto c : coefficients | boost::adaptors::reversed) {
             p = p * x + c;
         }
         return p;
     }
 
-    double operator[] (const ssize_t i) const {
-        return coefficients[i];
+    double operator[] (const size_t i) const {
+        return i > degree() ? 0 : coefficients[i];
     }
 
-    size_t size () const {
-        return coefficients.size();
+    size_t degree () const {
+        return coefficients.size() - 1;
     }
 
     polynomial derivative () const {
-        if (coefficients.size() <= 1) {
-            return polynomial { std::vector<double>() };
+        if (!degree()) {
+            return polynomial { { 0 } };
         }
 
-        std::vector<double> cs { coefficients.begin(), coefficients.end() - 1 };
-        for (int i = 0; i < cs.size(); i++) {
-            cs[i] *= cs.size() - i;
+        std::vector<double> dpdx { coefficients.cbegin() + 1, coefficients.cend() };
+
+        double power = 1.0;
+        for (auto& c : dpdx) {
+            c *= power++;
         }
         
-        return polynomial { cs };
+        return polynomial { dpdx };
     }
 
-    std::pair<polynomial, polynomial> operator/ (polynomial divisor) const {
-        int rsize = divisor.size() - 1;
-        int qsize = coefficients.size() - rsize;
+    template <typename DIter, typename QIter>
+    static auto ip (DIter dbegin, DIter dend, QIter qbegin, QIter qend) -> decltype(*dbegin * *qbegin) {
+        decltype(*dbegin * *qbegin) value = 0;
+        while (dbegin != dend && qbegin != qend) {
+            value += *dbegin * *qbegin;
+            ++dbegin;
+            ++qbegin;
+        }
+        return value;
+    }
 
-        std::vector<double> remainder { coefficients.begin() + qsize, coefficients.end() };
-        std::vector<double> quotient { coefficients.begin(), coefficients.begin() + qsize };
+    std::pair<polynomial, polynomial> quo_rem (polynomial divisor) const {
+        /* Degrees of the remainder and quotient. */
+        int kd = divisor.degree();
+        int kr = kd - 1;
+        int kq = degree() - kd;
 
-        /* FIXME rewrite this more functionally, with lists and shit */
+        std::vector<double> remainder (kr + 1);
+        std::vector<double> quotient (kq + 1);
 
-        for (int i = 0; i < quotient.size(); i++) {
-            for (int j = 1; j < divisor.size(); j++) {
-                if (i - j >= 0) {
-                    quotient[i] -= divisor[j] * quotient[i-j];
-                }
-            }
-            quotient[i] /= divisor[0];
+        auto dbegin = divisor.coefficients.crbegin() + 1;
+        auto dend = divisor.coefficients.crend();
+        auto qbegin = quotient.end();
+        auto qend = quotient.end();
+
+        for (int i = kq; 0 <= i; i--) {
+            quotient[i] = (coefficients[kd+i] - ip(dbegin, dend, qbegin, qend)) / divisor[kd];
+            --qbegin;
         }
 
-        for (int i = 0; i < remainder.size(); i++) {
-            for (int j = i+1; j < divisor.size(); j++) {
-                if (qsize + i - j >= 0) {
-                    remainder[i] -= divisor[j] * quotient[qsize + i - j];
-                }
-            }
+        for (int i = kr; 0 <= i; i--) {
+            remainder[i] = coefficients[i] - ip(dbegin, dend, qbegin, qend);
+            ++dbegin;
         }
 
         return { polynomial { quotient }, polynomial { remainder } };
+    }
+
+    const polynomial operator/ (polynomial divisor) const {
+        return quo_rem(divisor).first;
+    }
+
+    const polynomial operator% (polynomial divisor) const {
+        return quo_rem(divisor).second;
+    }
+
+    const polynomial operator+ (polynomial operand) const {
+        std::vector<double> result (std::max(degree(), operand.degree()) + 1);
+
+        for (int i = 0; i < result.size(); i++) {
+            double x = i <= degree() ? coefficients[i] : 0.0;
+            double y = i <= operand.degree() ? operand[i] : 0.0;
+            result[i] = x + y;
+        }
+
+        return polynomial { result };
+    }
+
+    const polynomial operator* (polynomial operand) const {
+        std::vector<double> result (degree() + operand.degree() + 1, 0.0);
+
+        for (int i = 0; i < coefficients.size(); i++) {
+            for (int j = 0; j < operand.coefficients.size(); j++) {
+                result[i+j] += coefficients[i] * operand.coefficients[j];
+            }
+        }
+
+        return polynomial { result };
+    }
+
+    const polynomial operator- (polynomial operand) const {
+        return *this + -operand;
+    }
+
+    const polynomial operator- () const {
+        return -1.0 * *this;
+    }
+
+    friend const polynomial operator* (double x, polynomial operand) {
+        for (int i = 0; i < operand.coefficients.size(); i++) {
+            operand.coefficients[i] *= x;
+        }
+        return operand;
+    }
+
+    friend const polynomial operator* (polynomial operand, double x) {
+        return x * operand;
     }
 
     friend std::ostream& operator<< (std::ostream& os, const polynomial& p) {
@@ -76,5 +143,18 @@ public:
 private:
     std::vector<double> coefficients;
 };
+
+std::vector<polynomial> sturm_chain (polynomial p) {
+    std::vector<polynomial> chain (p.degree() + 1);
+
+    chain[0] = p;
+    chain[1] = p.derivative();
+
+    for (int i = 2; i < chain.size(); i++) {
+        chain[i] = -(chain[i-2] % chain[i-1]);
+    }
+
+    return chain;
+}
 
 #endif
